e4ff0b2c514f1c489a1ff4a47f46c52b1290c356
Muhammad Raihan Satrio <adewipo@gmail.com>
2022-03-03 08:38:02 +0700

    chore: CI/CD

--- Repositories/IPengurusRepository.cs
+++ Repositories/IPengurusRepository.cs
@@ -5,9 +5,6 @@ namespace ApiPengurus.Repositories
     public interface IPengurusRepository
     {
         void AddPengurus(Pengurus pengurus);
-        // void UpdatePengurus(Pengurus pengurus);
-        // void DeletePengurus(string id);
         Pengurus GetPengurusSingleRecord(string nim);
-        // List<Pengurus> GetAllPengurus();
     }
 }
